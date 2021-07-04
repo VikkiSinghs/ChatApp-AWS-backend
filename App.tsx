c080e150a98af8548876765b23555edce14488ff
vikkisinghs <svkram46@gmail.com>
2021-07-04 15:55:34 +0530

    API and user modal implemented

--- App.tsx
+++ App.tsx
@@ -1,19 +1,77 @@
 import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { withAuthenticator } from 'aws-amplify-react-native'
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
+
+import {
+  Auth,
+  API,
+  graphqlOperation,
+} from 'aws-amplify';
+import {getUser} from './src/graphql/queries';
+import {createUser} from './src/graphql/mutations';
 import Amplify from "aws-amplify";
 import awsExports from "./src/aws-exports";
 Amplify.configure(awsExports);
 
+const randomImages = [
+  'https://hieumobile.com/wp-content/uploads/avatar-among-us-2.jpg',
+  'https://hieumobile.com/wp-content/uploads/avatar-among-us-3.jpg',
+  'https://hieumobile.com/wp-content/uploads/avatar-among-us-6.jpg',
+  'https://hieumobile.com/wp-content/uploads/avatar-among-us-9.jpg',
+]
+
 function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
+
+  const getRandomImage = () => {
+    return randomImages[Math.floor(Math.random() * randomImages.length)];
+  }
+
+  // run this snippet only when App is first mounted
+  useEffect( () => {
+    const fetchUser = async () => {
+      const userInfo = await Auth.currentAuthenticatedUser({ bypassCache: true });
+
+      if (userInfo) {
+        const userData = await API.graphql(
+          graphqlOperation(
+            getUser,
+            { id: userInfo.attributes.sub }
+            )
+        )
+
+        if (userData.data.getUser) {
+          console.log("User is already registered in database");
+          return;
+        }
+
+        const newUser = {
+          id: userInfo.attributes.sub,
+          name: userInfo.username,
+          imageUri: getRandomImage(),
+          status: 'Hey, I am using Indi Chat',
+        }
+
+        await API.graphql(
+          graphqlOperation(
+            createUser,
+            { input: newUser }
+          )
+        )
+      }
+    }
+
+    fetchUser();
+  }, [])
+
+
 
   if (!isLoadingComplete) {
     return null;
