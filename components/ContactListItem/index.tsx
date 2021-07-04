759220220199a69f668f439ebbe6d5b9532984ec
vikkisinghs <svkram46@gmail.com>
2021-07-04 21:12:38 +0530

    UserList chat implemented

--- components/ContactListItem/index.tsx
+++ components/ContactListItem/index.tsx
@@ -5,6 +5,15 @@ import moment from "moment";
 import styles from "./style";
 // import {Users} from "../data/Users";
 import { useNavigation } from '@react-navigation/native';
+import {
+    API,
+    graphqlOperation,
+    Auth,
+  } from "aws-amplify";
+  import {
+    createChatRoom,
+    createChatRoomUser
+  } from '../../src/graphql/mutations';
 
 
 export type ContactListItem = {
@@ -15,29 +24,72 @@ const ContactListItem = (props: ContactListItem) => {
 
     const navigation = useNavigation();
 
-    const onClick = () => {
-        //navigate to chatroom with this user
-    }
+    const onClick = async () => {
+        try {
+    
+          //  1. Create a new Chat Room
+          const newChatRoomData = await API.graphql(
+            graphqlOperation(
+              createChatRoom, {
+                input: { }
+              }
+            )
+          )
+    
+          if (!newChatRoomData.data) {
+            console.log(" Failed to create a chat room");
+            return;
+          }
+    
+          const newChatRoom = newChatRoomData.data.createChatRoom;
+    
+          // 2. Add `user` to the Chat Room
+          await API.graphql(
+            graphqlOperation(
+              createChatRoomUser, {
+                input: {
+                  userID: user.id,
+                  chatRoomID: newChatRoom.id,
+                }
+              }
+            )
+          )
+    
+          //  3. Add authenticated user to the Chat Room
+          const userInfo = await Auth.currentAuthenticatedUser();
+          await API.graphql(
+            graphqlOperation(
+              createChatRoomUser, {
+                input: {
+                  userID: userInfo.attributes.sub,
+                  chatRoomID: newChatRoom.id,
+                }
+              }
+            )
+          )
+    
+          navigation.navigate('ChatRoom', {
+            id: newChatRoom.id,
+            name: "Hardcoded name",
+          })
+    
+        } catch (e) {
+          console.log(e);
+        }
+}
 
-       
-    return (
-        <TouchableWithoutFeedback onPress={onClick}>
-        <View style = {styles.container}>
-                <View style={styles.lefContainer}>
-                    <Image source={{ uri:user.imageUri }} style={styles.avatar} />
-
-                    <View style={styles.midContainer}>
-                        <Text style={styles.username}>{user.name}</Text>
-                        <Text numberOfLines={2} style={styles.status}>{user.status}</Text>
-                         
-                    </View>
-                </View>
-
+return (
+    <TouchableWithoutFeedback onPress={onClick}>
+      <View style={styles.container}>
+        <View style={styles.lefContainer}>
+          <Image source={{ uri: user.imageUri }} style={styles.avatar}/>
+          <View style={styles.midContainer}>
+            <Text style={styles.username}>{user.name}</Text>
+            <Text numberOfLines={2} style={styles.status}>{user.status}</Text>
+          </View>
         </View>
-        </TouchableWithoutFeedback>
-    )
-
+      </View>
+    </TouchableWithoutFeedback>
+  )
 };
-
-
 export default ContactListItem;
